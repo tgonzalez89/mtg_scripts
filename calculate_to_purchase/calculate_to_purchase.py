b4bfa4bd3a27fe6d0d6ccfc0ac0d5ddf3df6b8b9
Tomas Gonzalez Aragon <tomas.gonz.arg@gmail.com>
2025-09-10 20:45:42 +0200

    owned decks: filter cards to improve avoided

--- calculate_to_purchase/calculate_to_purchase.py
+++ calculate_to_purchase/calculate_to_purchase.py
@@ -11,6 +11,9 @@ buy_considering = True
 deck_ids: tuple[str | tuple[str, ...], ...] = (
     # PASTE MOXFIELD DECK IDS HERE
 )
+owned_deck_ids: tuple[str | tuple[str, ...], ...] = (
+    # PASTE MOXFIELD DECK IDS HERE
+)
 
 # Get purchased cards.
 # Populate the purchased.txt file.
@@ -24,14 +27,14 @@ pattern = re.compile(r"(.+?)\n\t\n\w+ (\d+)\n\t\1", re.DOTALL)
 matches = pattern.findall(text)
 for match in matches:
     card_name = match[0].strip()
-    number = match[1].strip()
+    amount = match[1].strip()
     card_name = re.sub(r"(.*?[^/]) *//? *([^/].*)", r"\1 // \2", card_name).lower()
-    purchased_cards[card_name] = purchased_cards.get(card_name, 0) + int(number)
+    purchased_cards[card_name] = purchased_cards.get(card_name, 0) + int(amount)
 
 # json.dump(purchased_cards, Path("purchased.json").open("w"), indent=2, sort_keys=True)
 
 # Get owned cards.
-# Populate the owned.txt file. Format: number of cards and card name.
+# Populate the owned.txt file. Format: amount of cards and card name.
 
 owned_cards: dict[str, int] = {}
 with Path("owned.txt").open() as f:
@@ -40,11 +43,24 @@ for line in lines:
     line = line.strip()
     if not line:
         continue
-    number, card_name = line.split(" ", maxsplit=1)
+    amount, card_name = line.split(" ", maxsplit=1)
     card_name = re.sub(r"(.*?[^/]) *//? *([^/].*)", r"\1 // \2", card_name).lower()
-    owned_cards[card_name] = owned_cards.get(card_name, 0) + int(number)
+    owned_cards[card_name] = owned_cards.get(card_name, 0) + int(amount)
 
 # json.dump(owned_cards, Path("owned.json").open("w"), indent=2, sort_keys=True)
+
+# Calculate the cards that I have.
+
+have = copy.copy(owned_cards)
+for card_name, amount in purchased_cards.items():
+    if card_name not in have:
+        have[card_name] = amount
+    else:
+        have[card_name] += amount
+
+# json.dump(have, Path("have.json").open("w"), indent=2, sort_keys=True)
+
+# Get decks
 
 
 def download_deck(deck_id) -> tuple[dict[str, int], dict[str, int]]:
@@ -88,7 +104,8 @@ def download_deck(deck_id) -> tuple[dict[str, int], dict[str, int]]:
 
 cards_in_decks: dict[str, int] = {}
 considering_cards: dict[str, int] = {}
-for deck_id in deck_ids:
+# decks: dict[str, dict[str, int]] = {}
+for deck_id in deck_ids + owned_deck_ids:
     if isinstance(deck_id, tuple):
         main_decks = []
         maybeboards = []
@@ -108,98 +125,102 @@ for deck_id in deck_ids:
         main_deck, maybeboard = download_deck(deck_id)
 
     for card_name, amount in main_deck.items():
-        cards_in_decks[card_name] = cards_in_decks.get(card_name, 0) + amount
+        if deck_id in owned_deck_ids and card_name in have:
+            if have[card_name] > amount:
+                # I have more than I need for the deck, subtract from have and don't add to cards_in_deck because I don't need copies of this card for this deck.
+                have[card_name] = have[card_name] - amount
+            elif have[card_name] == amount:
+                # I have the exact amount I need for the deck, remove card from have and don't add to cards_in_deck because I don't need copies of this card for this deck.
+                have.pop(card_name)
+            else:
+                # I don't have enough copies, remove card from have and add to cards_in_deck the amount minus what I have (what I actually need).
+                cards_in_decks[card_name] = cards_in_decks.get(card_name, 0) + amount - have[card_name]
+                have.pop(card_name)
+        else:
+            cards_in_decks[card_name] = cards_in_decks.get(card_name, 0) + amount
+        # decks[str(deck_id)][card_name] = decks.get(str(deck_id), {}).get(card_name, 0) + amount
     for card_name, amount in maybeboard.items():
-        considering_cards[card_name] = considering_cards.get(card_name, 0) + amount
+        if deck_id in owned_deck_ids and card_name in have:
+            if amount > have[card_name]:
+                # I don't have enough copies, remove card from have and add to cards_in_deck the amount minus what I have (what I actually need).
+                considering_cards[card_name] = considering_cards.get(card_name, 0) + amount - have[card_name]
+            # else: simply don't add to considering, this way the card will not be taken into account, since we already have it
+            #       this helps to not pollute the 'avoided' list
+        else:
+            considering_cards[card_name] = considering_cards.get(card_name, 0) + amount
 
 
-json.dump(cards_in_decks, Path("in_decks.json").open("w"), indent=2, sort_keys=True)
-json.dump(considering_cards, Path("considering.json").open("w"), indent=2, sort_keys=True)
-
-# Calculate the cards that I have.
-
-have = copy.copy(owned_cards)
-for card_name, number in purchased_cards.items():
-    if card_name not in have:
-        have[card_name] = number
-    else:
-        have[card_name] += number
-
-json.dump(have, Path("have.json").open("w"), indent=2, sort_keys=True)
+# json.dump(cards_in_decks, Path("in_decks.json").open("w"), indent=2, sort_keys=True)
+# json.dump(considering_cards, Path("considering.json").open("w"), indent=2, sort_keys=True)
 
 # Calculate the cards that I need.
 
-need_all = copy.copy(cards_in_decks)
+need = copy.copy(cards_in_decks)
 need_decks = copy.copy(cards_in_decks)
 need_considering = {}
 if buy_considering:
-    for card_name, number in considering_cards.items():
-        if card_name not in need_all:
+    for card_name, amount in considering_cards.items():
+        if card_name not in need:
             # Only add 1 copy of each card from considering and only if it is not already in the decks.
-            need_all[card_name] = 1
+            need[card_name] = 1
             need_considering[card_name] = 1
 
-# json.dump(need_all, Path("need_all.json").open("w"), indent=2, sort_keys=True)
-json.dump(need_decks, Path("need_decks.json").open("w"), indent=2, sort_keys=True)
-json.dump(need_considering, Path("need_considering.json").open("w"), indent=2, sort_keys=True)
+# json.dump(need, Path("need.json").open("w"), indent=2, sort_keys=True)
+# json.dump(need_decks, Path("need_decks.json").open("w"), indent=2, sort_keys=True)
+# json.dump(need_considering, Path("need_considering.json").open("w"), indent=2, sort_keys=True)
 
-# Calculate the cards that I need to buy.
+# Calculate the cards that I need to buy by filtering out cards that I own or already purchased from the cards that I need.
 
 to_purchase = {}
 avoided = {}
 avoided_owned = {}
 avoided_purchased = {}
-for card_name, number in need_all.items():
+for card_name, amount in need.items():
     if card_name not in have:
-        to_purchase[card_name] = number
-    elif number > have[card_name]:
-        to_purchase[card_name] = number - have[card_name]
+        to_purchase[card_name] = amount
+    elif amount > have[card_name]:
+        to_purchase[card_name] = amount - have[card_name]
         avoided[card_name] = have[card_name]
         if card_name in purchased_cards:
             avoided_purchased[card_name] = purchased_cards[card_name]
         if card_name in owned_cards:
             avoided_owned[card_name] = owned_cards[card_name]
     else:
-        avoided[card_name] = number
-        left = number
+        avoided[card_name] = amount
+        left = amount
         if card_name in purchased_cards:
-            avoided_purchased[card_name] = min(number, purchased_cards[card_name])
-        if card_name in owned_cards and number - avoided_purchased.get(card_name, 0) > 0:
-            avoided_owned[card_name] = number - avoided_purchased.get(card_name, 0)
+            avoided_purchased[card_name] = min(amount, purchased_cards[card_name])
+        if card_name in owned_cards and amount - avoided_purchased.get(card_name, 0) > 0:
+            avoided_owned[card_name] = amount - avoided_purchased.get(card_name, 0)
+
+json.dump(avoided, Path("avoided.json").open("w"), indent=2, sort_keys=True)
+# json.dump(avoided_owned, Path("avoided_owned.json").open("w"), indent=2, sort_keys=True)
+# json.dump(avoided_purchased, Path("avoided_purchased.json").open("w"), indent=2, sort_keys=True)
 
 with Path("to_purchase.txt").open("w") as f:
-    for card_name, number in sorted(to_purchase.items()):
-        f.write(f"{number} {card_name}\n")
-
-# json.dump(to_purchase, Path("to_purchase.json").open("w"), indent=2, sort_keys=True)
-json.dump(avoided, Path("avoided.json").open("w"), indent=2, sort_keys=True)
-json.dump(avoided_owned, Path("avoided_owned.json").open("w"), indent=2, sort_keys=True)
-json.dump(avoided_purchased, Path("avoided_purchased.json").open("w"), indent=2, sort_keys=True)
+    for card_name, amount in sorted(to_purchase.items()):
+        f.write(f"{amount} {card_name}\n")
 
 to_purchase_decks = {}
-for card_name, number in need_decks.items():
+for card_name, amount in need_decks.items():
     if card_name not in have:
-        to_purchase_decks[card_name] = number
+        to_purchase_decks[card_name] = amount
     else:
-        if have[card_name] < number:
-            to_purchase_decks[card_name] = number - have[card_name]
+        if have[card_name] < amount:
+            to_purchase_decks[card_name] = amount - have[card_name]
 
 with Path("to_purchase_decks.txt").open("w") as f:
-    for card_name, number in sorted(to_purchase_decks.items()):
-        f.write(f"{number} {card_name}\n")
-
-# json.dump(to_purchase_decks, Path("to_purchase_decks.json").open("w"), indent=2, sort_keys=True)
+    for card_name, amount in sorted(to_purchase_decks.items()):
+        f.write(f"{amount} {card_name}\n")
 
 to_purchase_considering = {}
-for card_name, number in need_considering.items():
+for card_name, amount in need_considering.items():
     if card_name not in have:
-        to_purchase_considering[card_name] = number
+        to_purchase_considering[card_name] = amount
     else:
-        if have[card_name] < number:
-            to_purchase_considering[card_name] = number - have[card_name]
+        if have[card_name] < amount:
+            to_purchase_considering[card_name] = amount - have[card_name]
 
 with Path("to_purchase_considering.txt").open("w") as f:
-    for card_name, number in sorted(to_purchase_considering.items()):
-        f.write(f"{number} {card_name}\n")
-
-# json.dump(to_purchase_considering, Path("to_purchase_considering.json").open("w"), indent=2, sort_keys=True)
+    for card_name, amount in sorted(to_purchase_considering.items()):
+        f.write(f"{amount} {card_name}\n")
