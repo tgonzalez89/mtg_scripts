3157c8212d72ba76df6371f82c0f7a6a27896db5
Tomas Gonzalez Aragon <tomas.gonz.arg@gmail.com>
2025-10-21 18:05:04 +0200

    cleanup code and old stuff

--- cardmarket_optimizer/optimizer.py
+++ cardmarket_optimizer/optimizer.py
@@ -26,27 +26,6 @@ for card_name, amount in card_list.items():
                 "cards_available": 0,
             }
         sellers_db_cards_available[str(offer["seller"])]["cards_available"] += selected_amount
-# sellers_db_cards_available_sorted_1 = sorted(
-#     sellers_db_cards_available.items(), key=lambda x: (x[1]["shipping_price"], -x[1]["cards_available"])
-# )
-# sellers_db_cards_available_sorted_2 = sorted(
-#     sellers_db_cards_available.items(), key=lambda x: (-x[1]["cards_available"], x[1]["shipping_price"])
-# )
-# sellers_db_cards_available_sorted_dict_1 = dict(
-#     sellers_db_cards_available_sorted_1[: math.ceil(len(sellers_db_cards_available_sorted_1) / 2)]
-# )
-# sellers_db_cards_available_sorted_dict_2 = dict(
-#     sellers_db_cards_available_sorted_2[: math.ceil(len(sellers_db_cards_available_sorted_2) / 10)]
-# )
-# sellers_db_cards_available_sorted_dict = {
-#     k: sellers_db_cards_available_sorted_dict_1[k]
-#     for k in sellers_db_cards_available_sorted_dict_1.keys() & sellers_db_cards_available_sorted_dict_2.keys()
-# }
-# top_10_sellers = dict(
-#     sorted(
-#         sellers_db_cards_available_sorted_dict.items(), key=lambda x: (-x[1]["cards_available"], x[1]["shipping_price"])
-#     )[:10]
-# )
 
 
 def calc_total_prices(selected_offers: dict[str, list[dict[str, int | float | str]]]):
@@ -144,12 +123,8 @@ def run_algo(
 
 
 selected_offers, selected_sellers = run_algo(card_list, offers_database)
-json.dump(selected_offers, Path("selected_offers.json").open("w"), indent=2, sort_keys=True)
-total_price, items_price, shipping_price, sellers = calc_total_prices(selected_offers)
-print(f"{total_price=} {items_price=} {shipping_price=} {len(sellers)=}")
 
-
-# Get the sellers with only 1 item and try to see if we can reassign that item to a different-already-selected seller
+# Get the sellers with only 1 item and try to see if we can reassign that item to a different, but already-selected seller
 # so that we can save the shipping of the current seller.
 
 
@@ -174,43 +149,8 @@ for seller, offers in selected_offers_by_seller.items():
                         selected_offers.pop(card_name)
 
 offers_database = json.load(Path("offers_database.json").open())
-# Filter offers_database to have offers only from sellers in the previously selected offers.
-# for card_name, offers in tuple(offers_database.items()):
-#     filtered_offers = [offer for offer in offers if str(offer["seller"]) in selected_offers_by_seller]
-#     offers_database[card_name] = filtered_offers
 
 selected_offers, selected_sellers = run_algo(new_card_list, offers_database, selected_offers, selected_sellers)
 json.dump(selected_offers, Path("selected_offers.json").open("w"), indent=2, sort_keys=True)
 total_price, items_price, shipping_price, sellers = calc_total_prices(selected_offers)
 print(f"{total_price=} {items_price=} {shipping_price=} {len(sellers)=}")
-
-
-# sellers_with_1_item = sorted(
-#     seller
-#     for seller, offers in selected_offers_by_seller.items()
-#     if sum(int(offer["selected_amount"]) for offer in offers) == 1
-# )
-# print(sellers_with_1_item)
-
-# offers_database = json.load(Path("offers_database.json").open())
-# offers_database_by_seller: dict[str, dict[str, list[dict[str, int | float | str]]]] = {}
-# for card_name, offers in offers_database.items():
-#     for offer in offers:
-#         if offer["seller"] not in offers_database_by_seller:
-#             offers_database_by_seller[str(offer["seller"])] = {}
-#         if card_name not in offers_database_by_seller[str(offer["seller"])]:
-#             offers_database_by_seller[str(offer["seller"])][card_name] = []
-#         offers_database_by_seller[str(offer["seller"])][card_name].append(offer)
-
-
-# for seller_with_1_item in sellers_with_1_item:
-#     for seller, offers_by_card in offers_database_by_seller.items():
-#         for card_name, offers in offers_by_card.items():
-#             for offer in offers:
-#                 pass
-
-
-# NOOOOOO. Remove them from selected_offers and selected_sellers and repeat the algorithm with a new created card_list.
-
-# TODO: p2 Do a extra processing for those sellers with small number of cards and check in cardmarket.com if any of the selected sellers have those cards.
-# Then add them to the offers and run again p1.
